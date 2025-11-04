7aedac54b3937d73fcac2d3212ed6550dcf5a9ac
Hemant Nag <104633997+dev-hemant63@users.noreply.github.com>
2025-11-04 17:30:52 +0530

    Add project files.

---
+++ GoogleOAuth/Controllers/AccountController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GoogleOAuth.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet("login")]
+        public IActionResult Login()
+        {
+            var redirectUrl = Url.Action("GoogleResponse", "Account");
+            var properties = new AuthenticationProperties { RedirectUri = redirectUrl };
+            return Challenge(properties, "Google");
+        }
+
+        [HttpGet("google-response")]
+        public async Task<IActionResult> GoogleResponse()
+        {
+            var result = await HttpContext.AuthenticateAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            var claims = result.Principal?.Identities.FirstOrDefault()?.Claims
+                .Select(claim => new { claim.Type, claim.Value });
+            return Json(claims);
+        }
+
+        [HttpGet("logout")]
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return Redirect("/");
+        }
+    }
+}
